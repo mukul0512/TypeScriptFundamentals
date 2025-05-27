f37a7eae4b24d083c3717a3fe3b6e9c6cbc2c7b6
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-05-27 21:40:27 +0530

    igniting typeScript globally and created intro.ts file, understanding need of ts

---
+++ intro.js
@@ -0,0 +1,4 @@
+var user = { name: "Mukul Karnwal", age: 25 };
+console.log("Mukul Karnwal");
+console.log(user.name);
+console.log(user.age);
---
+++ intro.ts
@@ -0,0 +1,6 @@
+const user = {name: "Mukul Karnwal", age: 25};
+
+console.log("Mukul Karnwal");
+console.log(user.name);
+console.log(user.age);
+// console.log(user.email); // This will throw an error because 'email' is not defined in the user object
